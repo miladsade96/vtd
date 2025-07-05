57dafbc9270a8459fafcedbced4009a74acaa118
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-07-05 17:54:04 +0330

    add usingOldSchoolThreads method to demonstrate traditional thread usage

--- src/main/java/VirtualThreadsDemo.java
+++ src/main/java/VirtualThreadsDemo.java
@@ -2,4 +2,11 @@ package main.java;
 
 public class VirtualThreadsDemo {
 
+    public static void usingOldSchoolThreads() throws InterruptedException {
+        for (int i = 0; i < 50000; i++) {
+            Thread thread = new Thread(SampleRunnable.task());  // 38280 ms in my machine
+            thread.start();
+            thread.join();
+        }
+
 }
